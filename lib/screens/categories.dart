d71431a48d57e995ec23fda170f8e834c6e3b85f
Brandon Arnold <brandonarnold926@gmail.com>
2023-10-10 10:22:12 -0400

    Initial check in

---
+++ lib/screens/categories.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key, required this.availableMeals});
+
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    // List will only display dummyMeals that are contained in the meal.categories
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      // Two Columns
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2, // 1.5
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
